c40b93a75ad64d104d22e23cf3ae928344919975
Johnnyruz <johnnyruzdev01@outlook.com>
2019-10-12 21:21:51 +0000

    Update Install

--- install_latest.sh
+++ install_latest.sh
@@ -18,23 +18,11 @@ sudo rm -rf /usr/local/lib/python2.7/dist-packages/*
 #RUN INSTALL FOR FIRST TIME
 bash -c /home/pi/mypkgs/install_part1.sh
 
-#FIX ISSUE WITH libmysqlclient-dev
-sudo sed -i 's/libmysqlclient-dev/default-libmysqlclient-dev/' /usr/local/lib/python2.7/dist-packages/kegberry/app.py
-
-#IF BUSTER RELEASE - FIX ISSUE WITH MYSQL SERVER
-if sudo cat /etc/os-release | grep -q buster
-then
- sudo sed -i 's/mysql-server/default-mysql-server/' /usr/local/lib/python2.7/dist-packages/kegberry/app.py
-fi
-
-#RUN INSTALL FOR SECOND TIME
-bash -c /home/pi/mypkgs/install_part1.sh
-
 #FIX ISSUE WITH MYSQL PERMISSION DENIED
 sudo cp /home/pi/mypkgs/NEW-50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo systemctl restart mysql
 
-#RUN INSTALL FOR THIRD TIME
+#RUN INSTALL FOR SECOND TIME
 bash -c /home/pi/mypkgs/install_part1.sh
 
 #FIX ISSUE WITH HTTPS REQUIRED
