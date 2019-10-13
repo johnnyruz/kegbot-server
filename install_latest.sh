7d15b803f02992855a969326182506fb9660961c
Johnnyruz <johnnyruzdev01@outlook.com>
2019-10-13 17:35:19 +0000

    Update Install Script

--- install_latest.sh
+++ install_latest.sh
@@ -6,7 +6,7 @@ set +e
 #update system packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install -y wget unzip git
+sudo apt-get install -y wget unzip git curl
 
 #Get Required Files from Github
 wget https://github.com/johnnyruz/kegbot-server/raw/add_my_pkgs_to_github/my_kegbot_packages_latest.zip
@@ -37,4 +37,15 @@ fi
 #RUN INSTALL FOR THE FOURTH AND FINAL TIME
 bash -c /home/pi/mypkgs/install_part1.sh
 
+
+#INSTALL NODE SERVER
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+cd ~
+git clone --branch latest https://github.com/johnnyruz/kegbot-server.git
+sudo cp -rf ~/kegbot-server/node_server /home/kegbot/kegbot-server.venv/
+sudo cp -rf ~/kegbot-server/node_server/kegbot.conf /etc/supervisor/conf.d/kegbot.conf
+sudo rm -rf ~/kegbot-server
+sudo supervisorctl restart kegbot:*
 
