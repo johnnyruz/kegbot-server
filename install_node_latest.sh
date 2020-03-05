79db83525912666c4f6b30d0b651b535d176c16e
johnnyruz <johnnyruz@users.noreply.github.com>
2020-03-04 21:07:28 -0500

    Point to Buster Branch

--- install_node_latest.sh
+++ install_node_latest.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y nodejs
 sudo rm -rf /home/kegbot/kegbot-server.venv/node_server
 
 cd ~
-git clone --branch latest https://github.com/johnnyruz/kegbot-server.git
+git clone --branch latest_buster https://github.com/johnnyruz/kegbot-server.git
 sudo cp -rf ~/kegbot-server/node_server /home/kegbot/kegbot-server.venv/
 sudo cp -rf ~/kegbot-server/node_server/kegbot.conf /etc/supervisor/conf.d/kegbot.conf
 sudo rm -rf ~/kegbot-server
